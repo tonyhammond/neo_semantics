67a099932eda721b911cb6d609e9336405e96fa3
Tony Hammond <tony@tonyhammond.org>
2019-04-15 10:34:24 +0100

    Initial commit of neo_semantics

---
+++ mix.exs
@@ -0,0 +1,36 @@
+defmodule NeoSemantics.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :neo_semantics,
+      version: "0.1.0",
+      elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {NeoSemantics.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+
+      # property graphs
+      {:bolt_sips, "~> 1.5"},
+
+      # rdf graphs
+      {:sparql_client, "~> 0.2"},
+      {:hackney, "~> 1.15"}
+    ]
+  end
+
+end
