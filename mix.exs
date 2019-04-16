ac4b15adfd553c5af201a4fc92418d0f30d02552
Tony Hammond <tony@tonyhammond.org>
2019-04-16 11:03:06 +0100

    Added docs

--- mix.exs
+++ mix.exs
@@ -14,6 +14,8 @@ defmodule NeoSemantics.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+
       {:bolt_sips, "~> 1.5"},
       {:hackney, "~> 1.15"},
       {:tesla, "~> 1.2"}
